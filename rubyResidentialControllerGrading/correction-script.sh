0a6dc2a4d41d941f68e2e8592931707df0052954
MathieuHoude <mathieu.houde@codeboxx.biz>
2020-11-15 21:57:38 -0500

    First version of ruby controller grading api

--- rubyResidentialControllerGrading/correction-script.sh
+++ rubyResidentialControllerGrading/correction-script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-GITHUBHANDLE=$1
-
+rm rspec-results.json
 git clone git@github.com:$GITHUBHANDLE/Rocket_Elevators_Controllers.git
 mv Rocket_Elevators_Controllers/residential_controller.rb lib/
-rspec --format j << rspec-results.json
+rspec --format j >> rspec-results.json
+rm lib/residential_controller.rb
+rm -rf Rocket_Elevators_Controllers
